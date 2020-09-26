ba42dd960be8135cd4e225cd89b23660d1f45ee7
Marta Sousa <marta.sousa@gmail.com>
2020-09-26 18:29:14 -0300

    simplified code on render message

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -13,53 +13,50 @@ export default class Movies extends Component {
 		this.setState({ movies })
 	}
 
-	// renderMessage() {
-	// 	if (this.state.movies.length === 0) return <p>There are no movies</p>
-	// 	if (this.state.movies.length === 1) return <p>There is one movie</p>
-	// 	return <p>There are {this.state.movies.length} movies</p>
-	// }
-
-	render() {
-
+	renderMessage() {
 		const { length: count } = this.state.movies
 
 		if (count === 0) return <p>There are no movies</p>
+		if (count === 1) return <p>There is one movie</p>
+		return <p>There are {count} movies</p>
+	}
 
+	render() {
 		return (
 			<>
-				<p>There are {count} movies</p>
-
-				<table className="table">
-					<thead>
-						<tr>
-							<th scope="col">Title</th>
-							<th scope="col">Genre</th>
-							<th scope="col">Stock</th>
-							<th scope="col">Rate</th>
-							<th scope="col"></th>
-						</tr>
-					</thead>
-					<tbody>
-						{this.state.movies.map(movie =>
-							<tr key={movie._id}>
-								<td>{movie.title}</td>
-								<td>{movie.genre.name}</td>
-								<td>{movie.numberInStock}</td>
-								<td>{movie.dailyRentalRate}</td>
-								<td>
-									<button
-										onClick={() => this.handleDelete(movie)}
-										type="button"
-										className="btn btn-danger"
-									>
-										Delete
-                  </button>
-								</td>
+				{this.renderMessage()}
+				{this.state.movies.length > 0 &&
+					<table className="table">
+						<thead>
+							<tr>
+								<th scope="col">Title</th>
+								<th scope="col">Genre</th>
+								<th scope="col">Stock</th>
+								<th scope="col">Rate</th>
+								<th scope="col"></th>
 							</tr>
-						)}
-					</tbody>
-				</table>
-
+						</thead>
+						<tbody>
+							{this.state.movies.map(movie =>
+								<tr key={movie._id}>
+									<td>{movie.title}</td>
+									<td>{movie.genre.name}</td>
+									<td>{movie.numberInStock}</td>
+									<td>{movie.dailyRentalRate}</td>
+									<td>
+										<button
+											onClick={() => this.handleDelete(movie)}
+											type="button"
+											className="btn btn-danger"
+										>
+											Delete
+                  </button>
+									</td>
+								</tr>
+							)}
+						</tbody>
+					</table>
+				}
 			</>
 
 		);
